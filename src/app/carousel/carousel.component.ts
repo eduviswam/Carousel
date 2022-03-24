afb153cd514cf9b4983af4d1f6c2da1e1adf1103
eduviswam <eduviswam@gmail.com>
2022-03-24 17:15:32 +0000

    Initial commit

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,68 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductService } from '../service/apiservice/product.service';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
+
+export class CarouselComponent implements OnInit {
+  
+  errorMessage = '';
+  sub!: Subscription;
+  products: any = [];
+  scrollPosition = 0;
+  carouselWidth: number = 0;
+  cardWidth: number = 0;
+
+  @ViewChild('carouselDiv') carouselDiv: ElementRef | undefined;
+  @ViewChild('inner') carousel: ElementRef | undefined;
+  @ViewChild('innerItem') carouselItem: ElementRef | undefined;
+
+  constructor(private apiService: ProductService) { }
+
+  ngOnInit(): void {
+    this.sub = this.apiService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onNextClick(): void {
+    this.carouselWidth = this.carousel!.nativeElement.scrollWidth;
+    this.cardWidth = this.carouselItem!.nativeElement.clientWidth;
+
+    if (this.scrollPosition < (this.carouselWidth - this.cardWidth)) {
+      this.scrollPosition += this.cardWidth;
+      this.carousel!.nativeElement.scrollLeft += this.scrollPosition;
+      //this.carousel!.nativeElement.scrollLeft.animate(this.getScrollAnimation(), this.getScrollAnimationTiming());     
+    }
+  }
+
+  onPrevClick(): void {
+    this.carouselWidth = this.carousel!.nativeElement.scrollWidth;
+    this.cardWidth = this.carouselItem!.nativeElement.clientWidth;
+
+    if (this.scrollPosition > 0) {
+      this.scrollPosition -= this.cardWidth;
+      this.carousel!.nativeElement.scrollLeft -= this.scrollPosition;
+      //this.carousel!.nativeElement.scrollLeft.animate(this.getScrollAnimation(), this.getScrollAnimationTiming());
+    }
+  }
+
+  getScrollAnimation() {
+    return [
+      { scrollLeft: this.scrollPosition }
+    ];
+  }
+
+  getScrollAnimationTiming() {
+    return {
+      duration: 600
+    };
+  }
+}
